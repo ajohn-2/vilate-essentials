d99f333c5eccb8fe868a7adf6e2d0e8831f0efc6
Arion John <ajohn@brooklynsteamcenter.org>
2024-12-18 10:50:58 -0500

    Draft 3

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
+import flask_login
+
 import pymysql 
+
 from dynaconf import Dynaconf
 
 app = Flask(__name__)
 conf = Dynaconf(
     settings_file = ["settings.toml"]
-    
 )
+
+
+app = Flask(__name__)
+app.secret_key = conf.secret_key
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+
+    def __init__(self, user_id, username, email, name):
+        self.id = user_id
+        self.username = username
+        self.email = email
+        self.name = name
+
+    def get_id(self):
+        return str(self.id)
+    
+@login_manager.user_loader
+def load_user(user_id):
+    conn = connect_db
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Customer` WHERE `id` = {user_id};")
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    if result is not None:
+        return User(result["id"], result["username"], result["email"], result["name"])
+    
+
+    
+
+
 
 def connect_db():
     conn = pymysql.connect(
@@ -56,6 +97,68 @@ def productpage(product_id):
     conn.close()
     return render_template("product.html.jinja", product = result)
     
-@app.route("/signup")
+@app.route("/signup", methods=["POST", "GET"])
 def signuppage():
-    return render_template("signup.html.jinja")
+     if request.method == "POST":
+        name = request.form["name"]
+        username = request.form["username"]
+        password = request.form["pass"]
+        confirmpassword = request.form["confirmpass"]
+        email = request.form["email"]
+        address = request.form["address"]
+        conn = connect_db()
+        cursor = conn.cursor()
+        if len(password) < 8:
+            flash("Password contains less than 8 characters")
+        if confirmpassword != password:
+            flash("The passwords don't match")
+        else:
+            try:
+                cursor.execute(f"""
+                INSERT INTO `Customer` 
+                    (`first_name`, `last_name`, `username`, `password`, `email`, `address`)
+                VALUE
+                    ('{name}', '{username}', '{password}', '{email}', '{address}');
+                """)
+            except pymysql.err.IntegrityError:
+                flash("Username/Email is already in use")
+            else:    
+                return redirect("/signin") 
+            finally:
+                cursor.close()
+                conn.close()
+     return render_template("signup.html.jinja")
+    
+
+@app.route("/signin", methods=["POST", "GET"])
+def signinpage():
+    if request.method == "POST":
+        username = request.form['username'].strip()
+        password = request.form['password']
+
+        conn = connect_db()
+        cursor = conn.cursor()
+
+        cursor.execute(f"SELECT * FROM `Customer` WHERE `username` = '{username}';")
+        
+        result = cursor.fetchone()
+
+        if result is None: 
+            flash("Your username/password is incorrect")
+        elif password != result["password"]:
+            flash("Your username/password is incorrect")
+        else: 
+            user = User(result["id"], result["username"], result["email"], result["name"])
+
+            flask_login.login_user(user)
+
+            return redirect('/')
+
+       
+    return render_template("signin.html.jinja")
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect('/')
